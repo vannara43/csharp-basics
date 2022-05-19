8efd1c781d288287a77e5985f6ffe8c63c13fd1f
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-05-19 03:54:30 -0700

    added NonGenericCollections

---
+++ CSharp/NonGenericCollections.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class NonGenericCollections
+    {
+        public static void start()
+        {
+            Hashtable table = new Hashtable();
+            table.Add("UserName","user");
+            table.Add("Password", "passcode123");
+            table.Add("Button", "Submit");
+
+            foreach (var key in table.Keys)
+            {
+                Console.WriteLine("The Value for " + key + "is : " + table[key]);
+            }
+        }
+    }
+}
