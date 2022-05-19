965ec2b8bccbc4f8e4ab6d62e51550e363d82242
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-05-19 06:18:36 -0700

    added CustomTypeClass

---
+++ CSharp/CustomTypeClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
