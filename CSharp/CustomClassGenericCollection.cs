3005fc1fe65e80245ccba5d7861fb44b90f364bc
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-05-20 03:37:14 -0700

    added custom class collection

--- CSharp/CustomClassGenericCollection.cs
+++ CSharp/CustomClassGenericCollection.cs
@@ -11,7 +11,11 @@ namespace CSharp
         public void start()
         {
             List<User> users = new List<User>();
-            users.Add(new User());
+            users.Add(new User {UserID = 1, Name = "Alex", Age = 10, Email = "hisemail@email.com", Phone = 213214122 });
+            users.Add(new User { UserID = 2, Name = "Becca", Age = 20, Email = "hisemail@email.com", Phone = 213214122 });
+            users.Add(new User { UserID = 3, Name = "Charlie", Age = 30, Email = "hisemail@email.com", Phone = 213214122 });
+
+
         }
     }
 }
